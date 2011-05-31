80fe3460b01f2c538d71a42f3107d9fd41ab47dd
Daguan Lu <daguanlu@gmail.com>
2011-05-30 19:40:04 -0700

    Rails Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xeno2::Application.config.secret_token = 'b658cad3263aa60d91fb61420cf64ecc2dbeae2d851efa0a69c2ed4bf7ec79fe108db9d4a767437721dd844fd3f80608c5e733c5290d59d47d2737a7fda59b46'
